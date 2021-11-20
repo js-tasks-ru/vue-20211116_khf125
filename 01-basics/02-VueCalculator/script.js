258efdc5a3f0c426a389e46a816043cce35c45eb
Vladimir Maslov <vm85@inbox.ru>
2021-11-20 17:45:19 +1000

    задачи 1.1, 1.2, 1.3
    счетчик, калькулятор, маркированный список

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,26 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const RootComponent = defineComponent({
+  name: 'Root',
+
+  data() {
+    return {
+      firstValue: 0,
+      secondValue: 0,
+      operation: 'sum',
+    };
+  },
+  computed: {
+    resultValue() {
+      if (this.operation === 'sum') return this.firstValue + this.secondValue;
+      else if (this.operation === 'subtract') return this.firstValue - this.secondValue;
+      else if (this.operation === 'multiply') return this.firstValue * this.secondValue;
+      else if (this.operation === 'divide') return this.firstValue / this.secondValue;
+      else return 'Неизвестная операция';
+    },
+  },
+});
+
+const app = createApp(RootComponent);
+
+const vm = app.mount('#app');
